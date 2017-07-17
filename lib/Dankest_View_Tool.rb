686d3bd57e665f2f9615c4b25091c571bedd8099
Jordan Hudgens <jordan@devcamp.com>
2017-07-17 15:08:49 +0000

    danktool intial commit

---
+++ lib/Dankest_View_Tool.rb
@@ -0,0 +1,6 @@
+require "Dankest_View_Tool/version"
+require "Dankest_View_Tool/renderer"
+
+module DankestViewTool
+  # Your code goes here...
+end
---
+++ lib/Dankest_View_Tool/version.rb
@@ -0,0 +1,3 @@
+module DankestViewTool
+  VERSION = "0.1.0"
+end
